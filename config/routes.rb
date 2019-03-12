867e323f50c39c17cf00016c3569dfb628689c3e
Chris W Jones <chris.jones@cbsinteractive.com>
2019-03-12 18:08:21 -0400

    Adds sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/failure'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "welcome/index"
   root "welcome#index"
+
+  get   '/login', :to => 'sessions#new', :as => :login
+  match '/auth/:provider/callback', :to => 'sessions#create'
+  match '/auth/failure', :to => 'sessions#failure'
 
   resources :books
 
